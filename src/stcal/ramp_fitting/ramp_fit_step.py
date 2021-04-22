28666e308b7f76bee77ef2bd7a35f5b4b1dbf45e
Ken MacDonald <kmacdonald@stsci.edu>
2021-04-22 15:03:01 -0400

    Adding ramp fitting from JWST pipeline.

---
+++ src/stcal/ramp_fitting/ramp_fit_step.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+from . import ramp_fit
+
+__all__ = ["RampFitStep"]
+
+
+class RampFitStep:
+
+    """
+    This step fits a straight line to the value of counts vs. time to
+    determine the mean count rate for each pixel.
+    """
+    def process(self, input):
+        pass
