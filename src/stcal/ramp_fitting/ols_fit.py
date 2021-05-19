c2548559f4839296838115d3790fbe9e0bc9d328
Ken MacDonald <kmacdonald@stsci.edu>
2021-05-19 08:34:23 -0400

    Reverting code that may not be necessary.

--- src/stcal/ramp_fitting/ols_fit.py
+++ src/stcal/ramp_fitting/ols_fit.py
@@ -861,7 +861,8 @@ def ramp_fit_slopes(input_model, gain_2d, readnoise_2d, save_opt, weighting):
                 rhi = cubeshape[1]
 
             # Skip data section if it is all NaNs
-            data_sect = np.float32(data[num_int, :, :, :])
+            # data_sect = np.float32(data[num_int, :, :, :])
+            data_sect = data[num_int, :, :, :]
             if np.all(np.isnan(data_sect)):
                 log.error('Current data section is all nans, so not processing the section.')
                 continue
@@ -1597,8 +1598,10 @@ def calc_slope(data_sect, gdq_sect, frame_time, opt_res, save_opt, rn_sect,
     err_2d_array = data_sect[0, :, :] * frame_time
 
     # Suppress, then re-enable, harmless arithmetic warnings
+    '''
     warnings.filterwarnings("ignore", ".*invalid value.*", RuntimeWarning)
     warnings.filterwarnings("ignore", ".*divide by zero.*", RuntimeWarning)
+    '''
     err_2d_array[err_2d_array < 0] = 0
     warnings.resetwarnings()
 
