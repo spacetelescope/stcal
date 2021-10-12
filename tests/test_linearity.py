1628c96d67172c2c45174d01e2647355627c8108
Clare Shanahan <cshanahan@stsci.edu>
2021-10-12 11:44:12 -0400

    common code for linearity correction (#55)
    * common code for linearity correction
    
    * removed unused import

---
+++ tests/test_linearity.py
@@ -0,0 +1,87 @@
+"""
+
+Unit tests for linearity correction
+
+"""
+
+import numpy as np
+
+from stcal.linearity.linearity import linearity_correction
+
+DQFLAGS = {'GOOD': 0, 'DO_NOT_USE': 1, 'SATURATED': 2, 'DEAD': 1024, 'HOT': 2048, 'NO_LIN_CORR': 1048576}
+
+
+def test_coeff_dq():
+    """Test linearity algorithm with random data ramp (does algorithm match expected algorithm)
+    also test a variety of dq flags and expected output """
+
+    # size of integration
+    nints = 1
+    ngroups = 160
+    xsize = 103
+    ysize = 102
+
+    # Create data array and group/pixel dq arrays
+    data = np.ones((nints, ngroups, ysize, xsize)).astype(np.float32)
+    pdq = np.zeros((ysize, xsize)).astype(np.uint32)
+    gdq = np.zeros((nints, ngroups, ysize, xsize)).astype(np.uint32)
+
+    # Create reference file data and dq arrays
+    numcoeffs = 5
+    lin_coeffs = np.zeros((numcoeffs, ysize, xsize))
+
+    # Set coefficient values in reference file to check the algorithm
+    # Equation is DNcorr = L0 + L1*DN(i) + L2*DN(i)^2 + L3*DN(i)^3 + L4*DN(i)^4
+    # DN(i) = signal in pixel, Ln = coefficient from ref file
+    # L0 = 0 for all pixels for CDP6
+    L0 = 0.0e+00
+    L1 = 0.85
+    L2 = 4.62E-6
+    L3 = -6.16E-11
+    L4 = 7.23E-16
+
+    coeffs = np.asfarray([L0, L1, L2, L3, L4])
+    lin_coeffs[:, 30, 50] = coeffs
+    lin_dq = np.zeros((ysize, xsize), dtype=np.uint32)
+
+    # check behavior with NaN coefficients: should not alter pixel values
+    coeffs2 = np.asfarray([L0, np.nan, L2, L3, L4])
+    lin_coeffs[:, 20, 50] = coeffs2
+    data[0, 50, 20, 50] = 500.0
+
+    tgroup = 2.775
+
+    # set pixel values (DN) for specific pixels up the ramp
+    data[0, :, 30, 50] = np.arange(ngroups) * 100 * tgroup
+
+    scival = 40000.0
+    data[0, 45, 30, 50] = scival  # to check linearity multiplication is done correctly
+    data[0, 30, 35, 36] = 35  # pixel to check that dq=2 meant no correction was applied
+
+    # check if dq flags in pixeldq are correctly populated in output
+    pdq[50, 40] = DQFLAGS['DO_NOT_USE']
+    pdq[50, 41] = DQFLAGS['SATURATED']
+    pdq[50, 42] = DQFLAGS['DEAD']
+    pdq[50, 43] = DQFLAGS['HOT']
+
+    # set dq flags in DQ of reference file
+    lin_dq[35, 35] = DQFLAGS['DO_NOT_USE']
+    lin_dq[35, 36] = DQFLAGS['NO_LIN_CORR']
+    lin_dq[30, 50] = DQFLAGS['GOOD']
+
+    np.bitwise_or(pdq, lin_dq)
+
+    # run linearity correction
+    output_data, output_pdq = linearity_correction(data, gdq, pdq, lin_coeffs, lin_dq, DQFLAGS)
+
+    # check that multiplication of polynomial was done correctly for specified pixel
+    outval = L0 + (L1 * scival) + (L2 * scival**2) + (L3 * scival**3) + (L4 * scival**4)
+    assert(np.isclose(output_data[0, 45, 30, 50], outval, rtol=0.00001))
+
+    # check that dq value was handled correctly
+    assert output_pdq[35, 35] == DQFLAGS['DO_NOT_USE']
+    assert output_pdq[35, 36] == DQFLAGS['NO_LIN_CORR']
+    # NO_LIN_CORR, sci value should not change
+    assert output_data[0, 30, 35, 36] == 35
+    # NaN coefficient should not change data value
+    assert output_data[0, 50, 20, 50] == 500.0
