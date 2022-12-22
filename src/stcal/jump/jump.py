b091c68f86139cc02ef240bfaae015afeef30a74
Zach Burnett <zburnett@stsci.edu>
2022-12-22 14:51:19 -0500

    fix installation instructions

--- src/stcal/jump/jump.py
+++ src/stcal/jump/jump.py
@@ -13,8 +13,7 @@ try:
 
     ELLIPSE_PACKAGE = 'opencv-python'
 except (ImportError, ModuleNotFoundError):
-    ELLIPSE_PACKAGE_WARNING = '`opencv-python` must be installed (`pip install jwst[jump_ellipses]`) ' \
-                              'in order to use ellipses'
+    ELLIPSE_PACKAGE_WARNING = '`opencv-python` must be installed (`pip install stcal[opencv]`) in order to use ellipses'
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
