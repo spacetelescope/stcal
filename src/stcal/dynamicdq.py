f7a41249059cb25a7c9ea616c19026514294fa26
Ken MacDonald <kmacdonald@stsci.edu>
2023-02-28 10:48:08 -0500

    Updating the dynamic_mask function in order to save models that use the 'dq_def' attribute to compress the DQ array.

--- src/stcal/dynamicdq.py
+++ src/stcal/dynamicdq.py
@@ -6,7 +6,7 @@ log.setLevel(logging.DEBUG)
 log.addHandler(logging.NullHandler())
 
 
-def dynamic_mask(input_model, mnemonic_map):
+def dynamic_mask(input_model, mnemonic_map, inv=False):
     """
     Return a mask model given a mask with dynamic DQ flags.
 
@@ -16,7 +16,13 @@ def dynamic_mask(input_model, mnemonic_map):
     ----------
     input_model : ``MaskModel``
         An instance of a Mask model defined in jwst or romancal.
+
     mnemonic_map : dict
+        Dictionary of flag names and values.
+
+    inv : bool
+        If true, compress using the dq_def.  If false, decompress
+        using the dq_def.
 
     Returns
     -------
@@ -36,15 +42,26 @@ def dynamic_mask(input_model, mnemonic_map):
         for record in dq_table:
             bitplane = record['VALUE']
             dqname = record['NAME'].strip()
+
+            # Check that a flag in the 'dq_def' is a valid DQ flag.
             try:
                 standard_bitvalue = mnemonic_map[dqname]
             except KeyError:
                 log.warning('Keyword %s does not correspond to an existing '
                             'DQ mnemonic, so will be ignored' % (dqname))
                 continue
-            just_this_bit = np.bitwise_and(input_model.dq, bitplane)
-            pixels = np.where(just_this_bit != 0)
-            dqmask[pixels] = np.bitwise_or(dqmask[pixels], standard_bitvalue)
+
+            if not inv:
+                # Decompress the DQ array using 'dq_def'.
+                just_this_bit = np.bitwise_and(input_model.dq, bitplane)
+                pixels = np.where(just_this_bit != 0)
+                dqmask[pixels] = np.bitwise_or(dqmask[pixels], standard_bitvalue)
+            else:
+                # Compress the DQ array using 'dq_def'.
+                just_this_bit = np.bitwise_and(input_model.dq, standard_bitvalue)
+                pixels = np.where(just_this_bit != 0)
+                dqmask[pixels] = np.bitwise_or(dqmask[pixels], bitplane)
+                
     else:
         dqmask = input_model.dq
 
