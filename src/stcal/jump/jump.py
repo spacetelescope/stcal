b8db379f4f83873ede31baeaf898504754f9015d
Mike Regan <mregan@stsci.edu>
2023-03-09 13:03:05 -0500

    testing

--- src/stcal/jump/jump.py
+++ src/stcal/jump/jump.py
@@ -184,6 +184,7 @@ def detect_jumps(frames_per_group, data, gdq, pdq, err,
     pdq : int, 2D array
         updated pixel dq array
     """
+    print("Find Showers", find_showers)
     constants.update_dqflags(dqflags)  # populate dq flags
     sat_flag = dqflags["SATURATED"]
     jump_flag = dqflags["JUMP_DET"]
