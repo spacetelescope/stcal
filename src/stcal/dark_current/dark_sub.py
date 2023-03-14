6c9e61f59683de64a42c27cf053eef3f803c92e1
Mike Regan <mregan@stsci.edu>
2023-03-14 12:23:33 -0400

    Update dark_sub.py

--- src/stcal/dark_current/dark_sub.py
+++ src/stcal/dark_current/dark_sub.py
@@ -84,8 +84,10 @@ def do_correction_data(science_data, dark_data, dark_output=None):
 
     drk_nframes = dark_data.exp_nframes
     drk_groupgap = dark_data.exp_groupgap
-    drk_nframes = dark_data.data.shape[1]
+    drk_nframes = 1
     drk_groupgap = 0
+    drk_ngroups = 359
+    drk_nints = 2
 
     log.info(
         'Science data nints=%d, ngroups=%d, nframes=%d, groupgap=%d',
