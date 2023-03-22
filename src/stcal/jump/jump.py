76ae02cb344fe28ed0bc9ceed3a5fc4fef3dc86b
Mike Regan <mregan@stsci.edu>
2023-03-22 16:32:18 -0400

    Update jump.py

--- src/stcal/jump/jump.py
+++ src/stcal/jump/jump.py
@@ -6,6 +6,7 @@ from astropy.convolution import convolve
 import numpy as np
 from . import twopoint_difference as twopt
 from . import constants
+from astropy.io import fits
 
 import multiprocessing
 
@@ -329,7 +330,7 @@ def detect_jumps(frames_per_group, data, gdq, pdq, err,
             # save the neighbors to be flagged that will be in the next slice
             previous_row_above_gdq = row_above_gdq.copy()
             k += 1
-            
+
         fits.writeto("input_gdq_fle.fits", gdq, overwrite=True)
         #  This is the flag that controls the flagging of either snowballs or showers.
         if expand_large_events:
