ab435de6178936821f43758376d328eeacc26fa1
Mike Regan <mregan@stsci.edu>
2023-03-28 10:19:49 -0400

    Update twopoint_difference.py

--- src/stcal/jump/twopoint_difference.py
+++ src/stcal/jump/twopoint_difference.py
@@ -166,10 +166,10 @@ def find_crs(dataa, group_dq, read_noise, normal_rej_thresh,
                     sigma[np.newaxis, np.newaxis, :, :]
             log.info(" Jump Step using selfcal sigma clip {} greater than {}, rejection threshold {}".format(
                 str(total_groups), str(minimum_selfcal_groups), str(normal_rej_thresh)))
-            mean, median, stddev = stats.sigma_clipped_stats(np.abs(first_diffs_masked), sigma=normal_rej_thresh,
+            mean, median, stddev = stats.sigma_clipped_stats(first_diffs_masked, sigma=normal_rej_thresh,
                                                              axis=(0, 1))
             fits.writeto('stddev_sigclip.fits', stddev, overwrite=True)
-            clipped_diffs = stats.sigma_clip(np.abs(first_diffs_masked), sigma=normal_rej_thresh,
+            clipped_diffs = stats.sigma_clip(first_diffs_masked, sigma=normal_rej_thresh,
                                              axis=(0, 1), masked=True)
             max_diffs = np.nanmax(clipped_diffs, axis=(0, 1))
             min_diffs = np.nanmin(clipped_diffs, axis=(0, 1))
