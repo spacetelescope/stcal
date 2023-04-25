JP-3221: Fixing Segment Length Computation Needed for Optimal Weighting (#163)
* First pass at fixing optimal weighting.

Correcting the way the number of groups in a segment gets calculated for the optimal weighting during the weighted OLS fitting for each segment.

* Updating the change log.

* Updating the computation of num_nz to pass style checks.

* Updating the change log.

* Update CHANGES.rst

---------

Co-authored-by: Howard Bushouse <bushouse@stsci.edu>

tests/test_ramp_fitting.py
@@ -1156,7 +1156,7 @@ def test_new_saturation():
     # Check slopes information
     sdata, sdq, svp, svr, serr = slopes
 
-    check = np.array([[2.797567 , 2.8022935, np.nan]])
+    check = np.array([[2.795187 , 2.795632, np.nan]])
     np.testing.assert_allclose(sdata, check, tol, tol)
 
     check = np.array([[JUMP, JUMP, DNU | SAT]])
@@ -1174,8 +1174,8 @@ def test_new_saturation():
     # Check slopes information
     cdata, cdq, cvp, cvr, cerr = cube
 
-    check = np.array([[[2.7949152, 2.8022935, np.nan]],
-                      [[2.8020892, np.nan, np.nan]]])
+    check = np.array([[[2.7949152, 2.7956316, np.nan]],
+                      [[2.7956493, np.nan, np.nan]]])
     np.testing.assert_allclose(cdata, check, tol, tol)
 
     check = np.array([[[GOOD, JUMP, DNU | SAT]],