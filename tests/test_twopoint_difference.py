0e3476e09d67a3287d7bfaa52ed4327443b7ff24
Mike Regan <mregan@stsci.edu>
2023-06-01 14:17:21 -0400

    Update test_twopoint_difference.py

--- tests/test_twopoint_difference.py
+++ tests/test_twopoint_difference.py
@@ -1100,3 +1100,17 @@ def test_5grp_realTSO():
                  after_jump_flag_e2=0.0, after_jump_flag_n2=0,
                  copy_arrs=True, minimum_groups=3, minimum_selfcal_groups=15000)
     fits.writeto("new_gdq_cutout.fits", gdq, overwrite=True)
+
+def test_5grp_allTSO():
+    hdul = fits.open("obs2508_sigmaclip_base_dark_current.fits")
+    gdq = hdul['groupdq'].data
+    data = hdul['sci'].data
+    readnoise = 25
+    read_noise = np.full((gdq.shape[2], gdq.shape[3]), readnoise, dtype=np.float32)
+
+    gdq, row_below_gdq, row_above_gdq, total_primary_crs = \
+        find_crs(data, gdq, read_noise, 3, 4, 5, 1, False, 1000, 10, DQFLAGS,
+                 after_jump_flag_e1=0.0, after_jump_flag_n1=0,
+                 after_jump_flag_e2=0.0, after_jump_flag_n2=0,
+                 copy_arrs=True, minimum_groups=3, minimum_selfcal_groups=15000)
+    fits.writeto("new_sigma_clip_base.fits", gdq, overwrite=True)
