5aacab10800e5407c3e52735b787cded7279aec0
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2023-07-10 11:30:46 -0400

    update ramp fitting from romanisim memory efficiency work

--- src/stcal/ramp_fitting/ols_cas22_fit.py
+++ src/stcal/ramp_fitting/ols_cas22_fit.py
@@ -296,6 +296,7 @@ def fit_ramps_casertano(resultants, dq, read_noise, ma_table=None, read_pattern=
         minlength=npix)
     var.reshape(npix, 3, 2, 2)[:, 0, 1, 1] = (
         totval / (totweight ** 2 + (totweight == 0)))
+
     # poisson noise variances
     totval = np.bincount(
         rampfitdict['pix'],
@@ -303,7 +304,8 @@ def fit_ramps_casertano(resultants, dq, read_noise, ma_table=None, read_pattern=
     var.reshape(npix, 3, 2, 2)[..., 1, 1, 1] = (
         totval / (totweight ** 2 + (totweight == 0)))
 
-    var[..., 1, 1, 1] *= par[..., 1]  # multiply Poisson term by flux
+    # multiply Poisson term by flux.  Clip at zero; no negative Poisson variances.
+    var[..., 1, 1, 1] *= np.clip(par[..., 1], 0, np.inf)
     var[..., 2, 1, 1] = var[..., 0, 1, 1] + var[..., 1, 1, 1]
 
     if resultants.shape != origshape:
