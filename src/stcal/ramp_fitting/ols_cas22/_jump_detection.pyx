76eef893e786c77438341d4a0a7b3f2ac603e310
William Jamieson <wjamieson@stsci.edu>
2023-08-23 12:46:59 -0400

    Initial full jump algorithm

--- src/stcal/ramp_fitting/ols_cas22/_jump_detection.pyx
+++ src/stcal/ramp_fitting/ols_cas22/_jump_detection.pyx
@@ -7,10 +7,6 @@ cimport cython
 from stcal.ramp_fitting.ols_cas22._core cimport Ramp
 
 
-cdef float threshold(float intercept, float constant, float slope):
-    return intercept - constant * log10(slope)
-
-
 cdef class Jump(Ramp):
     """
     Class to contain the data for a single ramp fit with jump detection
@@ -60,7 +56,7 @@ cdef class Jump(Ramp):
     @cython.boundscheck(False)
     @cython.wraparound(False)
     @cython.cdivision(True)
-    cdef inline float statistic(Jump self, float slope):
+    cdef inline vector[float] statistic(Jump self, float slope):
         cdef int n_stats = len(self.n_reads), i
 
         cdef vector[float] stats = vector[float](n_stats)
@@ -72,4 +68,128 @@ cdef class Jump(Ramp):
 
             stats[i] = max(stat_1, stat_2)
 
-        return max(stats)
+        return stats
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    @cython.cdivision(True)
+    cdef inline (float, float, float, vector[float]) jump(Jump self):
+        cdef float slope, read_var, poisson_var 
+        slope, read_var, poisson_var = self.fit()
+
+        cdef vector[float] stats = self.statistic(slope)
+
+        return slope, read_var, poisson_var, stats
+
+    # @cython.boundscheck(False)
+    # @cython.wraparound(False)
+    # @cython.cdivision(True)
+    # cdef inline (Jump, Jump) split(Jump self, int split):
+    #     cdef Jump jump_1 = make_jump(
+    #             self.resultants, self.start, self.start + split, self.read_noise,
+    #             self.t_bar, self.tau, self.n_reads)
+
+    #     cdef Jump jump_2 = make_jump(
+    #             self.resultants, self.start + split + 2, self.end, self.read_noise,
+    #             self.t_bar, self.tau, self.n_reads)
+
+    #     return jump_1, jump_2
+
+
+
+
+cdef float threshold(float intercept, float constant, float slope):
+    return intercept - constant * log10(slope)
+
+
+cdef inline Jump make_jump(
+        float [:] resultants, int start, int end, float read_noise,
+        vector[float] t_bar, vector[float] tau, vector[int] n_reads):
+
+    """
+    Fast constructor for the Jump C class.
+
+    This is signifantly faster than using the `__init__` or `__cinit__`
+        this is because this does not have to pass through the Python as part
+        of the construction.
+
+    Parameters
+    ----------
+    resultants : float [:]
+        array of resultants for single pixel
+            - memoryview of a numpy array to avoid passing through Python
+    start : int
+        starting point of portion to fit within this pixel
+    end : int
+        ending point of portion to fit within this pixel
+    read_noise : float
+        read noise for this pixel
+    t_bar : vector[float]
+        mean times of resultants
+    tau : vector[float]
+        variance weighted mean times of resultants
+    n_reads : vector[int]
+        number of reads contributing to reach resultant
+
+    Return
+    ------
+    jump : Jump
+        Jump C-class object
+    """
+
+    cdef Jump jump = Jump()
+
+    jump.start = start
+    jump.end = end
+
+    jump.resultants = resultants
+    jump.t_bar = t_bar
+    jump.tau = tau
+
+    jump.read_noise = read_noise
+
+    jump.n_reads = n_reads
+
+    return jump
+
+
+cdef (vector[float], vector[float], vector[float]) fit(
+        float [:] resultants, int start, int end, float read_noise,
+        vector[float] t_bar, vector[float] tau, vector[int] n_reads,
+        float intercept, float constant):
+
+    cdef vector[float] slopes
+    cdef vector[float] read_vars
+    cdef vector[float] poisson_vars
+
+    cdef Jump jump, jump_1, jump_2
+    cdef int split
+    cdef vector[float] stats
+    cdef float slope, read_var, poisson_var
+
+    cdef list[Jump] jumps = [make_jump(resultants, start, end, read_noise, t_bar, tau, n_reads)]
+    while jumps:
+        jump = jumps.pop()
+        slope, read_var, poisson_var, stats = jump.jump()
+
+        if max(stats) > threshold(intercept, constant, slope):
+            split = np.argmax(stats)
+
+            jump_1 = make_jump(
+                    jump.resultants, jump.start, jump.start + split, jump.read_noise,
+                    jump.t_bar, jump.tau, jump.n_reads)
+
+            jump_2 = make_jump(
+                    jump.resultants, jump.start + split + 2, jump.end, jump.read_noise,
+                    jump.t_bar, jump.tau, jump.n_reads)
+
+            jumps.append(jump_1)
+            jumps.append(jump_2)
+
+        else:
+            slopes.push_back(slope)
+            read_vars.push_back(read_var)
+            poisson_vars.push_back(poisson_var)
+
+
+        return stats, read_vars, poisson_vars
