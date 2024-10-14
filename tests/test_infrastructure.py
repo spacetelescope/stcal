12c58863fdb6b05cdcfe17e2ef05d78f808c8d6f
Ned Molter <emolter@stsci.edu>
2024-10-14 15:26:14 -0400

    attempted fix test for oldest deps

--- tests/test_infrastructure.py
+++ tests/test_infrastructure.py
@@ -7,10 +7,10 @@ from stcal.testing_helpers import MemoryThreshold, MemoryThresholdExceeded
 
 def test_memory_threshold():
     with MemoryThreshold("10 KB"):
-        buff = np.empty(1000, dtype=np.uint8)
+        buff = np.ones(1000, dtype=np.uint8)
 
 
 def test_memory_threshold_exceeded():
     with pytest.raises(MemoryThresholdExceeded):
         with MemoryThreshold("500. B"):
-            buff = np.empty(1000, dtype=np.uint8)
+            buff = np.ones(10000, dtype=np.uint8)
