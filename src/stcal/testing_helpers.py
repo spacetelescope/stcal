2a7d03a8164eac5f2a28bb430578a109b45e69ad
Ned Molter <emolter@stsci.edu>
2024-10-14 09:31:21 -0400

    added back in a docstring change brett wanted

--- src/stcal/testing_helpers.py
+++ src/stcal/testing_helpers.py
@@ -13,6 +13,14 @@ class MemoryThreshold:
     example usage:
     with MemoryThreshold(expected_usage):
         # code that should not exceed expected
+
+    If the code in the with statement uses more than the expected_usage
+    memory (in bytes) a ``MemoryThresholdExceeded`` exception
+    will be raised.
+    
+    Note that this class does not prevent allocations beyond the threshold
+    and only checks the actual peak allocations to the threshold at the
+    end of the with statement.
     """
 
     def __init__(self, expected_usage, log_units="KB"):
