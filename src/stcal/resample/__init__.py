f95302a910760fd0bfa2bd6effe2df0c419df167
Mihai Cara <mihail.cara@gmail.com>
2024-12-12 21:23:34 -0500

    address reviewers' comments - part 1

--- src/stcal/resample/__init__.py
+++ src/stcal/resample/__init__.py
@@ -1,12 +1,10 @@
 from .resample import (
-    OutputTooLargeError,
     Resample,
     compute_wcs_pixel_area,
     UnsupportedWCSError,
 )
 
 __all__ = [
-    "OutputTooLargeError",
     "Resample",
     "compute_wcs_pixel_area",
     "UnsupportedWCSError",
