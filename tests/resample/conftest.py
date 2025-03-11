eddef9baebb90a7d6d9fb8b7d6c39cb2259abc7f
Mihai Cara <mihail.cara@gmail.com>
2025-03-11 16:39:36 -0400

    Do not rescale resampled image intensities

--- tests/resample/conftest.py
+++ tests/resample/conftest.py
@@ -1,7 +1,14 @@
 """ Test various utility functions """
 import pytest
+from pathlib import Path
+
+import asdf
+
+from stcal.alignment.util import wcs_bbox_from_shape
 
 from . helpers import make_gwcs
+
+DATADIR = "data"
 
 
 @pytest.fixture(scope='function')
@@ -11,3 +18,14 @@ def wcs_gwcs():
     shape = (1000, 1000)
     pscale = 0.06 / 3600
     return make_gwcs(crpix, crval, pscale, shape)
+
+
+@pytest.fixture()
+def nrcb5_wcs_wcsinfo():
+    """ Returns both wcs and wcsinfo """
+    path = Path(__file__).parent.parent / DATADIR / "nrcb5-wcs.asdf"
+    with asdf.open(path, lazy_load=False) as asdf_file:
+        wcs = asdf_file.tree["wcs"]
+        wcs.bounding_box = wcs_bbox_from_shape(wcs.array_shape)
+        wcsinfo = asdf_file.tree["wcsinfo"]
+        return wcs, wcsinfo
