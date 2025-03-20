f89b5cf7e741f69005d0a856583f6042654591b0
Mihai Cara <mihail.cara@gmail.com>
2025-03-20 01:34:35 -0400

    add unit test for the hook method

--- tests/resample/test_resample.py
+++ tests/resample/test_resample.py
@@ -1,15 +1,52 @@
 import pytest
 
+from drizzle.utils import calc_pixmap
 from stcal.resample import Resample
+from stcal.resample.utils import (
+    build_driz_weight,
+    resample_range,
+)
 
 import numpy as np
 
 from . helpers import (
     make_gwcs,
     make_input_model,
-    make_output_model,
     JWST_DQ_FLAG_DEF,
 )
+
+
+class _CustomResample(Resample):
+    def add_model_hook(self, model, pixmap, iscale, weight_map, xmin, xmax, ymin, ymax):
+
+        data = model["data"]
+        wcs = model["wcs"]
+        iscale_ref = self._get_intensity_scale(model)
+
+        pixmap_ref = calc_pixmap(
+            wcs,
+            self.output_model["wcs"],
+            data.shape,
+        )
+
+        weight_ref = build_driz_weight(
+            model,
+            weight_type=self.weight_type,
+            good_bits=self.good_bits,
+            flag_name_map=self.dq_flag_name_map
+        )
+
+        xyrange = resample_range(
+            data.shape,
+            wcs.bounding_box
+        )
+
+        assert np.allclose(pixmap_ref, pixmap)
+        assert np.allclose(weight_ref, weight_map)
+        assert np.allclose(xyrange, (xmin, xmax, ymin, ymax))
+        assert abs(iscale - iscale_ref) < 1.0e-6
+
+        raise RuntimeError("raised by subclass' add_model_hook")
 
 
 @pytest.mark.parametrize(
@@ -85,3 +122,44 @@ def test_resample_mostly_defaults(weight_type):
     assert np.nansum(resample.output_model["var_flat"]) > 0.0
     assert np.nansum(resample.output_model["var_poisson"]) > 0.0
     assert np.nansum(resample.output_model["var_rnoise"]) > 0.0
+
+
+def test_resample_add_model_hook():
+    crval = (150.0, 2.0)
+    crpix = (500.0, 500.0)
+    shape = (1000, 1000)
+    pscale = 0.06 / 3600
+
+    w = make_gwcs(
+        crpix=(600, 600),
+        crval=crval,
+        pscale=pscale,
+        shape=(1200, 1200)
+    )
+    output_wcs = {
+        "wcs": w,
+        "pixel_scale": pscale * 3600,
+    }
+
+    nmodels = 1
+
+    resample = _CustomResample(
+        n_input_models=nmodels,
+        output_wcs=output_wcs,
+        weight_type="exptime"
+    )
+    resample.dq_flag_name_map = JWST_DQ_FLAG_DEF
+
+    im = make_input_model(
+        shape=shape,
+        crpix=crpix,
+        crval=crval,
+        pscale=pscale,
+        group_id=1,
+        exptime=10.0
+    )
+
+    with pytest.raises(RuntimeError) as err_info:
+        resample.add_model(im)
+
+    assert str(err_info.value) == "raised by subclass' add_model_hook"
