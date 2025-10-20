52906b9f1edea64552547579c735dd565d99eb5e
Mihai Cara <mihail.cara@gmail.com>
2025-10-20 17:28:41 -0400

    Rescale resample kernels by pixel scale ratio - V2

--- tests/resample/test_resample.py
+++ tests/resample/test_resample.py
@@ -20,7 +20,8 @@ from .helpers import (
 
 
 class _CustomResample(Resample):
-    def add_model_hook(self, model, pixmap, iscale, weight_map, xmin, xmax, ymin, ymax):
+    def add_model_hook(self, model, pixmap, pixel_scale_ratio, iscale, weight_map, xmin, xmax, ymin, ymax):
+
         data = model["data"]
         wcs = model["wcs"]
         iscale_ref = self._get_intensity_scale(model)
