8291dadb59d1e479b9b92100189464f9d64ff49a
Timothy Brandt <tbrandt@astarlas2024.stsci.edu>
2025-12-09 00:35:32 -0500

    Added attributes to class

--- tests/test_jump.py
+++ tests/test_jump.py
@@ -47,6 +47,9 @@ def create_jump_data(dims, gain, rnoise, tm):
 
     frame_time, nframes, groupgap = tm
     jump_data.nframes = nframes
+    jump_data.dt_group = np.ones(1)
+    jump_data.n_reads_groupdiff = np.ones(1)*2*jump_data.nframes
+
 
     return jump_data
 
