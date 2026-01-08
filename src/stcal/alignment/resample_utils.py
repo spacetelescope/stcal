d406580fb070412d61ecb82d91891e491408eefb
Timothy Brandt <tbrandt@astarlas2024.stsci.edu>
2026-01-08 15:27:06 -0500

    Removed reproject functions, enabled interpolation in calc_pixmap

--- src/stcal/alignment/resample_utils.py
+++ src/stcal/alignment/resample_utils.py
@@ -1,6 +1,8 @@
 import logging
 
+import math
 import numpy as np
+from scipy import interpolate
 import shapely.geometry  # type: ignore[import-untyped]
 from gwcs.wcstools import grid_from_bounding_box
 
@@ -8,38 +10,167 @@ from stcal.alignment import util
 
 log = logging.getLogger(__name__)
 
-
-def calc_pixmap(in_wcs, out_wcs, shape=None):
+def calc_pixmap(wcs_from, wcs_to, shape=None, disable_bbox="to",
+                stepsize=10, order=3):
     """
-    Return a pixel grid map from input frame to output frame.
+    Calculate a discretized on a grid mapping between the pixels of two images
+    using provided WCS of the original ("from") image and the destination ("to")
+    image.
+
+    .. note::
+       This function assumes that output frames of ``wcs_from`` and ``wcs_to``
+       WCS have the same units.
 
     Parameters
     ----------
-    in_wcs : `~astropy.wcs.WCS`
-        Input WCS objects or transforms.
-    out_wcs : `~astropy.wcs.WCS` or `~gwcs.wcs.WCS`
-        output WCS objects or transforms.
-    shape : tuple, optional
-        Shape of grid in pixels. The default is None.
+    wcs_from : wcs
+        A WCS object representing the coordinate system you are
+        converting from. This object's ``array_shape`` (or ``pixel_shape``)
+        property will be used to define the shape of the pixel map array.
+        If ``shape`` parameter is provided, it will take precedence
+        over this object's ``array_shape`` value.
+
+    wcs_to : wcs
+        A WCS object representing the coordinate system you are
+        converting to.
+
+    shape : tuple, None, optional
+        A tuple of integers indicating the shape of the output array in the
+        ``numpy.ndarray`` order. When provided, it takes precedence over the
+        ``wcs_from.array_shape`` property.
+
+    disable_bbox : {"to", "from", "both", "none"}, optional
+        Indicates whether to use or not to use the bounding box of either
+        (both) ``wcs_from`` or (and) ``wcs_to`` when computing pixel map. When
+        ``disable_bbox`` is "none", pixel coordinates outside of the bounding
+        box are set to `NaN` only if ``wcs_from`` or (and) ``wcs_to`` sets
+        world coordinates to NaN when input pixel coordinates are outside of
+        the bounding box.
+
+    stepsize : int, optional
+        If ``stepsize>1``, perform the full WCS calculation on a sparser
+        grid and use interpolation to fill in the rest of the pixels.  This
+        option speeds up pixel map computation by reducing the number of WCS
+        calls, though at the cost of reduced pixel map accuracy.  The loss
+        of accuracy is typically negligible if the underlying distortion
+        correction is smooth, but if the distortion is non-smooth,
+        ``stepsize>1`` is not recommended.  Large ``stepsize`` values are
+        automatically reduced to no more than 1/10 of image size.
+        Default 1.
+
+    order : int, optional
+        Order of the 2D spline to interpolate the sparse pixel mapping
+        if stepsize>1.  Supported values are: 1 (bilinear) or 3 (bicubic).
+        This Parameter is ignored when ``stepsize <= 1``.  Default 1.
 
     Returns
     -------
-    pixmap : ndarray of shape (xdim, ydim, 2)
-        Reprojected pixel grid map. `pixmap[xin, yin]` returns `xout,
-        yout` indices in the output image.
-    """
-    if shape:
-        bb = util.wcs_bbox_from_shape(shape)
-        log.debug("Bounding box from data shape: %s", bb)
-    else:
-        bb = util.wcs_bbox_from_shape(in_wcs.pixel_shape)
-        log.debug("Bounding box from WCS: %s", bb)
+    pixmap : numpy.ndarray
+        A three dimensional array representing the transformation between
+        the two. The last dimension is of length two and contains the x and
+        y coordinates of a pixel center, respectively. The other two coordinates
+        correspond to the two coordinates of the image the first WCS is from.
 
-    # creates 2 grids, one with rows of all x values * len(y) rows,
-    # and the reverse for all y columns
-    grid = grid_from_bounding_box(bb)
-    transform_function = util.reproject(in_wcs, out_wcs)
-    return np.dstack(transform_function(grid[0], grid[1]))
+    Raises
+    ------
+    ValueError
+        A `ValueError` is raised when output pixel map shape cannot be
+        determined from provided inputs.
+
+    Notes
+    -----
+    When ``shape`` is not provided and ``wcs_from.array_shape`` is not set
+    (i.e., it is `None`), `calc_pixmap` will attempt to determine pixel map
+    shape from the ``bounding_box`` property of the input ``wcs_from`` object.
+    If ``bounding_box`` is not available, a `ValueError` will be raised.
+
+    """
+    if (bbox_from := getattr(wcs_from, "bounding_box", None)) is not None:
+        try:
+            # to avoid dependency on astropy just to check whether
+            # the bounding box is an instance of
+            # modeling.bounding_box.ModelBoundingBox, we try to
+            # directly use and bounding_box(order='F') and if it fails,
+            # fall back to converting the bounding box to a tuple
+            # (of intervals):
+            bbox_from = bbox_from.bounding_box(order="F")
+        except AttributeError:
+            bbox_from = tuple(bbox_from)
+
+    if (bbox_to := getattr(wcs_to, "bounding_box", None)) is not None:
+        try:
+            # to avoid dependency on astropy just to check whether
+            # the bounding box is an instance of
+            # modeling.bounding_box.ModelBoundingBox, we try to
+            # directly use and bounding_box(order='F') and if it fails,
+            # fall back to converting the bounding box to a tuple
+            # (of intervals):
+            bbox_to = bbox_to.bounding_box(order="F")
+        except AttributeError:
+            bbox_to = tuple(bbox_to)
+
+    if shape is None:
+        shape = wcs_from.array_shape
+        if shape is None and bbox_from is not None:
+            if (ndim := np.ndim(bbox_from)) == 1:
+                bbox_from = (bbox_from,)
+            if ndim > 1:
+                shape = tuple(math.ceil(lim[1] + 0.5) for lim in bbox_from[::-1])
+
+    if shape is None:
+        raise ValueError('The "from" WCS must have pixel_shape property set.')
+
+    # temporarily disable the bounding box for the "from" WCS:
+    if disable_bbox in ["from", "both"] and bbox_from is not None:
+        wcs_from.bounding_box = None
+    if disable_bbox in ["to", "both"] and bbox_to is not None:
+        wcs_to.bounding_box = None
+    try:
+        if stepsize == 1:
+            y, x = np.indices(shape, dtype=np.float64)
+            x, y = wcs_to.world_to_pixel_values(*wcs_from.pixel_to_world_values(x, y))
+        else:
+
+            if not order in [1, 3]:
+                raise ValueError("Interpolation order should be either 1 or 3.")
+
+            y_in, x_in = np.arange(shape[0]), np.arange(shape[1])
+
+            # Number of points so that step size is no larger than requested
+            # but at least 10 points are used (or the number of input pixels
+            # in each dimension, if smaller than that).
+
+            npts_x = max(int(math.ceil(shape[1] / stepsize)), min(10, shape[1]))
+            npts_y = max(int(math.ceil(shape[0] / stepsize)), min(10, shape[0]))
+
+            x_coarse = np.linspace(0, x_in[-1], npts_x)
+            y_coarse = np.linspace(0, y_in[-1], npts_y)
+
+            sparsegrid = np.meshgrid(x_coarse, y_coarse)
+
+            pixmap_coarse = wcs_to.world_to_pixel_values(
+                *wcs_from.pixel_to_world_values(sparsegrid[0], sparsegrid[1]))
+
+            fx = interpolate.RectBivariateSpline(x_coarse, y_coarse,
+                                                 pixmap_coarse[0],
+                                                 kx=order, ky=order)
+            fy = interpolate.RectBivariateSpline(x_coarse, y_coarse,
+                                                 pixmap_coarse[1],
+                                                 kx=order, ky=order)
+
+            # Evaluate the spline on the full grid
+
+            x = fx(x_in, y_in)
+            y = fy(x_in, y_in)
+
+    finally:
+        if bbox_from is not None:
+            wcs_from.bounding_box = bbox_from
+        if bbox_to is not None:
+            wcs_to.bounding_box = bbox_to
+
+    pixmap = np.dstack([x, y])
+    return pixmap
 
 
 def combine_sregions(sregion_list, det2world, intersect_footprint=None):
